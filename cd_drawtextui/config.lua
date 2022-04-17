ec28ddf6d92dd90a3ae136ff30305c5ed7cc0ef4
ItIsYeCat <86536680+ItIsYeCat@users.noreply.github.com>
2022-04-17 06:14:03 +0430

    Add files via upload

---
+++ cd_drawtextui/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+Config.Locations = {
+    [1] = {
+		coords = vector3(-62.54, -1074.29, 27.16),
+		distance = 4,
+		key = 311,
+		eventname = '',
+		text = '<b>Love You Trey</b>'
+	},
+	[2] = {
+		coords = vector3(-62.54, -1074.29, 27.16),
+		distance = 20,
+		key = 311,
+		eventname = '',
+		text = 'Parking'
+	},
+	[3] = {
+		coords = vector3(-263.67, -959.76, 31.22),
+		distance = 3,
+		key = 311,
+		eventname = '',
+		text = '[E] Enter Appartments|[G] Move Here'
+	},
+}
